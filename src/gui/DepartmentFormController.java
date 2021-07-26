ab1006bbc288ee4a4f3a36896d95b5eed05ff192
Victor Rossetto <victor_rossetto1@hotmail.com>
2021-07-26 14:27:14 -0300

    Passando o Departamento para o formulario

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,8 +9,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable{
+	
+	private Department entity;
+	
 	@FXML
 	private TextField txtId;
 	
@@ -25,6 +29,10 @@ public class DepartmentFormController implements Initializable{
 	
 	@FXML
 	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity=entity;
+	}
 	
 	@FXML
 	public void onBtSaveAction() {
@@ -49,6 +57,14 @@ public class DepartmentFormController implements Initializable{
 	private void initializeNodes() {
 		Constraints.setTextFieldDouble(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if(entity==null) {
+			throw new IllegalStateException("Entities was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 
 }
